4f5935e03e2347f30be86d83d4503db0c114c5c8
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-07-07 13:10:07 +0000

    [add]4-3 投稿一覧詳細画面追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2a0c9a202aa5f6894861a4cc56392f4d897928b7708b6393f3db6ca45d7123d0a125e09d23abb89ab20c07ad9483f5352299b8753ebed33b1896d99d514b0776'
