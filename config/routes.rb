9fa4740afb724f747b92e66e3a4e42cf09a99c78
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-07-08 03:47:10 +0000

    [finish]4-4

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:"homes#top"
   resources :post_images, only:[:new,:create,:index,:show,:destroy] do
+    # 詳細ページ等必要ないのでid不要。resource単数形にしている
+    resource :favorites,only:[:create,:destroy]
     resources :post_comments,only:[:create,:destroy]
   end
 end
